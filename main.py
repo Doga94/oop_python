3604dc2214766245e64bb549c7ae8edc617ed4fd
David Gomez <doga19941@hotmail.com>
2023-03-13 23:00:55 -0500

    Clases y Objetos

---
+++ main.py
@@ -0,0 +1,13 @@
+from persona import Persona
+
+persona1 = Persona()
+persona1.nombre = 'David'
+persona1.edad = 29
+
+persona2 = Persona()
+persona2.nombre = 'Orlando'
+persona2.edad = 25
+
+persona2.mostrar_datos()
+print('='*25)
+persona1.mostrar_datos()
---
+++ persona.py
@@ -0,0 +1,9 @@
+class Persona: #las clases inician en mayusculas
+    #atributos
+    nombre = None
+    edad = None
+
+    #metodos
+    def mostrar_datos(self):
+        print(f'Nombre: {self.nombre}')
+        print(f'Edad: {self.edad}')
